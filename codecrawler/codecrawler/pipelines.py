a998d3c31ce42640ad8687c0e83e5736d1edd0ce
BradyContracts <bradycontractsllc@gmail.com>
2025-03-30 05:08:06 +0000

    i

--- codecrawler/codecrawler/pipelines.py
+++ codecrawler/codecrawler/pipelines.py
@@ -1,55 +1,19 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
-import sqlite3
-
-class SQLitePipeline:
-    def open_spider(self, spider):
-        self.conn = sqlite3.connect("scraped_data.db")
-        self.cursor = self.conn.cursor()
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS code_data (
-                id INTEGER PRIMARY KEY,
-                code TEXT,
-                language TEXT,
-                title TEXT
-            )
-        ''')
-
-    def close_spider(self, spider):
-        self.conn.commit()
-        self.conn.close()
-
-    def process_item(self, item, spider):
-        self.cursor.execute('''
-            INSERT INTO code_data (code, language, title)
-            VALUES (?, ?, ?)
-        ''', (item["code"], item["language"], item["title"]))
-        return item
-
-
-class CodecrawlerPipeline:
-    def process_item(self, item, spider):
-        return item
 import re
+from scrapy.utils.html import remove_tags
 
 class CleanCodePipeline:
     def process_item(self, item, spider):
-        # Clean the 'code' field by removing any non-alphanumeric characters
+        # Clean the 'code' field by removing HTML tags and extra spaces
         if 'code' in item:
             item['code'] = self.clean_code(item['code'])
         return item
 
     def clean_code(self, code):
-        # Example cleaning function to remove non-alphanumeric characters
-        return re.sub(r"[^a-zA-Z0-9_]", "", code)  # This can be adjusted for more complex cleaning
+        # Remove HTML tags from the code
+        cleaned_code = remove_tags(code)
+        
+        # Optionally, clean up extra spaces, newlines, or non-alphanumeric characters
+        cleaned_code = re.sub(r'\s+', ' ', cleaned_code)  # Replace multiple spaces/newlines with a single space
+        cleaned_code = re.sub(r"[^a-zA-Z0-9_\-(),.]+", " ", cleaned_code)  # Remove unwanted special characters (but keep basic punctuation)
 
-    
-
-
+        return cleaned_code.strip()  # Return cleaned code without leading/trailing spaces
